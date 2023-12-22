05531c1a4137f6ad9a25866f549185e6c0fe27fa
maoif <maoif@foxmail.com>
2023-12-22 14:54:12 +0800

    TsmExpr: extend from TruffleObject to allow for interop.

--- trusteme-lib/src/main/java/org/maoif/trusteme/types/TsmExpr.java
+++ trusteme-lib/src/main/java/org/maoif/trusteme/types/TsmExpr.java
@@ -1,7 +1,9 @@
 package org.maoif.trusteme.types;
 
+import com.oracle.truffle.api.interop.TruffleObject;
+
 /**
  * Parent of all Trusteme types.
  */
-public abstract class TsmExpr {
+public abstract class TsmExpr implements TruffleObject {
 }
