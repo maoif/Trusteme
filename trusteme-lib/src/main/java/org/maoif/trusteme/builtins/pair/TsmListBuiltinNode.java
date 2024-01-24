666d566a1b2c21bdf3062094badf72b855f47e36
maoif <maoif@foxmail.com>
2024-01-24 21:20:54 +0800

    Finish vector and list builtin.

--- trusteme-lib/src/main/java/org/maoif/trusteme/builtins/pair/TsmListBuiltinNode.java
+++ trusteme-lib/src/main/java/org/maoif/trusteme/builtins/pair/TsmListBuiltinNode.java
@@ -2,15 +2,41 @@ package org.maoif.trusteme.builtins.pair;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 
+import com.oracle.truffle.api.nodes.NodeInfo;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
 import org.maoif.trusteme.builtins.TsmBuiltinNode;
+import org.maoif.trusteme.types.TsmExpr;
+import org.maoif.trusteme.types.TsmPair;
 
+@NodeInfo(shortName = "list")
 public class TsmListBuiltinNode extends TsmBuiltinNode {
-    protected TsmListBuiltinNode() {
+    public TsmListBuiltinNode() {
         super("list");
     }
 
     @Override
     public Object executeGeneric(VirtualFrame frame) {
-        return null;
+        return executeTsmPair(frame);
+    }
+
+    @Override
+    public TsmPair executeTsmPair(VirtualFrame frame) {
+        Object[] args = frame.getArguments();
+        if (args.length == 0)
+            throw new RuntimeException("Lexical scope is lost");
+        if (args.length == 1)
+            return new TsmPair();
+
+        TsmPair res = new TsmPair();
+        res.setCar((TsmExpr) args[1]);
+
+        TsmPair cdr = res;
+        for (int i = 2; i < args.length; i++) {
+            TsmPair p = new TsmPair((TsmExpr) args[i]);
+            cdr.setCdr(p);
+            cdr = p;
+        }
+
+        return res;
     }
 }
--- trusteme-lib/src/main/java/org/maoif/trusteme/builtins/vector/TsmVectorBuiltinNode.java
+++ trusteme-lib/src/main/java/org/maoif/trusteme/builtins/vector/TsmVectorBuiltinNode.java
@@ -1,16 +1,37 @@
-package org.maoif.trusteme.builtins;
+package org.maoif.trusteme.builtins.vector;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
 
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
+import org.maoif.trusteme.builtins.TsmBuiltinNode;
+import org.maoif.trusteme.types.TsmExpr;
+import org.maoif.trusteme.types.TsmVector;
+
 @NodeInfo(shortName = "vector")
 public class TsmVectorBuiltinNode extends TsmBuiltinNode {
-    protected TsmVectorBuiltinNode() {
+    public TsmVectorBuiltinNode() {
         super("vector");
     }
 
     @Override
     public Object executeGeneric(VirtualFrame frame) {
-        return null;
+        return executeTsmVector(frame);
+    }
+
+    @Override
+    public TsmVector executeTsmVector(VirtualFrame frame) {
+        Object[] args = frame.getArguments();
+        if (args.length == 0)
+            throw new RuntimeException("Lexical scope is lost");
+        if (args.length == 1)
+            return new TsmVector(0);
+
+        TsmVector v = new TsmVector(args.length - 1);
+        for (int i = 1; i < args.length; i++) {
+            v.rawArray()[i - 1] = (TsmExpr) args[i];
+        }
+
+        return v;
     }
 }
