6680e07c9774f1033cd3c28ca094a53e17ebab5f
maoif <maoif@foxmail.com>
2024-01-30 20:48:55 +0800

    Make TsmEq consider primitive types.

--- trusteme-lib/src/main/java/org/maoif/trusteme/builtins/TsmEq.java
+++ trusteme-lib/src/main/java/org/maoif/trusteme/builtins/TsmEq.java
@@ -4,7 +4,7 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
 
 import org.maoif.trusteme.builtins.TsmBuiltinNode;
-import org.maoif.trusteme.types.TsmBool;
+import org.maoif.trusteme.types.*;
 
 @NodeInfo(shortName = "eq?")
 public class TsmEq extends TsmBuiltinNode {
@@ -25,6 +25,41 @@ public class TsmEq extends TsmBuiltinNode {
         if (args.length != 3)
             throw new RuntimeException("invalid argument count in " + this.NAME);
 
-        return TsmBool.get(args[1] == args[2]);
+        var a = args[1];
+        var b = args[2];
+
+        if (a instanceof TsmBool x) {
+            if (b instanceof TsmBool y) {
+                return TsmBool.get(x == y);
+            } else {
+                return TsmBool.FALSE;
+            }
+        } else if (a instanceof TsmChar x) {
+            if (b instanceof TsmChar y) {
+                return TsmBool.get(x.get() == y.get());
+            } else {
+                return TsmBool.FALSE;
+            }
+        } else if (a instanceof TsmFixnum x) {
+            if (b instanceof TsmFixnum y) {
+                return TsmBool.get(x.get() == y.get());
+            } else {
+                return TsmBool.FALSE;
+            }
+        } else if (a instanceof TsmFlonum x) {
+            if (b instanceof TsmFlonum y) {
+                return TsmBool.get(x.get() == y.get());
+            } else {
+                return TsmBool.FALSE;
+            }
+        } else if (a instanceof TsmBignum x) {
+            if (b instanceof TsmBignum y) {
+                return TsmBool.get(x.get().equals(y.get()));
+            } else {
+                return TsmBool.FALSE;
+            }
+        } else {
+            return TsmBool.get(args[1] == args[2]);
+        }
     }
 }
