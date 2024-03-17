ce18bb1870fafdb1027465d08e2fffc56da9e6ee
maoif <maoif@foxmail.com>
2024-03-17 18:17:18 +0800

    TrustemeLanguage: update builtins, remove extra code, etc.

--- trusteme-lib/src/main/java/org/maoif/trusteme/TrustemeLanguage.java
+++ trusteme-lib/src/main/java/org/maoif/trusteme/TrustemeLanguage.java
@@ -13,17 +13,23 @@ import com.oracle.truffle.api.strings.TruffleString;
 import org.maoif.trusteme.builtins.*;
 import org.maoif.trusteme.builtins.bool.TsmIsBool;
 import org.maoif.trusteme.builtins.bool.TsmNot;
-import org.maoif.trusteme.builtins.conversions.TsmList2Vector;
+import org.maoif.trusteme.builtins.character.TsmCharLessThan1;
+import org.maoif.trusteme.builtins.character.TsmCharLessThanOrEqualTo1;
+import org.maoif.trusteme.builtins.character.TsmIsChar;
+import org.maoif.trusteme.builtins.conversions.*;
 import org.maoif.trusteme.builtins.hashtable.TsmHashtableRef;
 import org.maoif.trusteme.builtins.hashtable.TsmHashtableSet;
 import org.maoif.trusteme.builtins.hashtable.TsmMakeEqHashtable;
 import org.maoif.trusteme.builtins.io.*;
 import org.maoif.trusteme.builtins.io.text.*;
-import org.maoif.trusteme.builtins.number.TsmAddBuiltinNode;
+import org.maoif.trusteme.builtins.number.TsmIsNumber;
+import org.maoif.trusteme.builtins.number.TsmMod;
 import org.maoif.trusteme.builtins.number.TsmNumberEqualBuiltinNode;
+import org.maoif.trusteme.builtins.number.bignum.*;
 import org.maoif.trusteme.builtins.number.fixnum.*;
+import org.maoif.trusteme.builtins.number.flonum.*;
 import org.maoif.trusteme.builtins.pair.*;
-import org.maoif.trusteme.builtins.string.TsmIsString;
+import org.maoif.trusteme.builtins.string.*;
 import org.maoif.trusteme.builtins.vector.*;
 import org.maoif.trusteme.nodes.TsmEvalRootNode;
 import org.maoif.trusteme.nodes.TsmNode;
@@ -35,6 +41,7 @@ import org.maoif.trusteme.types.*;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -47,11 +54,11 @@ public class TrustemeLanguage extends TruffleLanguage<TrustemeContext> {
 
     public static final String ID = "tsm";
     public static final String MIME_TYPE = "application/x-tsm";
-    private static final Source BUILTIN_SOURCE = Source.newBuilder(TrustemeLanguage.ID, "", "TSM builtin").build();
-
     public static final TruffleString.Encoding STRING_ENCODING = TruffleString.Encoding.UTF_16;
 
-//    private final Parser parser = new Parser();
+    private static final String PSYNTAX = "psyntax.pp";
+    private static final String PRIMS = "prims.ss";
+
     private VirtualFrame topFrame = null;
     private ConcurrentMap<String, TsmExpr> topEnv = new ConcurrentHashMap<>();
 
@@ -72,7 +79,7 @@ public class TrustemeLanguage extends TruffleLanguage<TrustemeContext> {
         return parse(request.getSource().getCharacters().toString());
     }
 
-    protected CallTarget parse(String src) {
+    public CallTarget parse(String src) {
         var builder = FrameDescriptor.newBuilder();
         int lexicalSlot = builder.addSlots(1, FrameSlotKind.Object);
 
@@ -89,39 +96,52 @@ public class TrustemeLanguage extends TruffleLanguage<TrustemeContext> {
     }
 
     Set<TsmBuiltinNode> builtins = Set.of(
-            new TsmEvalCore(),
+            new TsmEvalCore(), new TsmCommandLine(), new TsmExit(),
             new TsmError(),
+            new TsmDisplayBuiltinNode(),
+            new TsmSymbolValue(), new TsmSetSymbolValue(),
 
             new TsmVoidBuiltinNode(),
             new TsmEq(), new TsmEqv(), new TsmEqual(),
 
             new TsmIsProcedure(),
 
-            new TsmAddBuiltinNode(),
-            new TsmNumberEqualBuiltinNode(),
+            new TsmIsNumber(), new TsmNumberEqualBuiltinNode(), new TsmMod(),
+            new TsmIsFixnum(),
 
             new TsmFixEqualBuiltinNode(), new TsmFixAddBuiltinNode(), new TsmFixSubBuiltinNode(), new TsmFixMulBuiltinNode(), new TsmFixDivBuiltinNode(),
             new TsmFixModBuiltinNode(), new TsmFixRemainderBuiltinNode(),
             new TsmFixLessThanBuiltinNode(), new TsmFixLessThanOrEqualBuiltinNode(),
 
+            new TsmIsFlonum(), new TsmFloAdd(), new TsmFloMul(), new TsmFloSub(), new TsmFloDiv(),
+
+            new TsmIsBignum(), new TsmBigAdd(), new TsmBigMul(), new TsmBigSub(), new TsmBigDiv(),
+
+            new TsmFixnum2Flonum(), new TsmFix2Big(),
+
             new TsmCarBuiltinNode(), new TsmCdrBuiltinNode(), new TsmConsBuiltinNode(),
             new TsmSetCar(), new TsmSetCdr(),
             new TsmListBuiltinNode(), new TsmIsList(), new TsmIsPair(), new TsmIsNull(),
-            new TsmLengthBuiltinNode(), new TsmListRef(), new TsmList2Vector(),
+            new TsmLengthBuiltinNode(), new TsmListRef(),
 
             new TsmIsBool(), new TsmNot(),
 
-            new TsmIsString(),
+            new TsmIsChar(), new TsmCharLessThan1(), new TsmCharLessThanOrEqualTo1(),
+            new TsmIsString(), new TsmMakeString(), new TsmStringRef(), new TsmStringSet(),
+            new TsmStringLength(), new TsmStringAppend(),
             new TsmIsSymbol(),
 
-            new TsmVectorBuiltinNode(),
+            new TsmNumber2String(), new TsmString2Symbol(), new TsmSymbol2String(),
+
+            new TsmVectorBuiltinNode(), new TsmMakeVector(),
             new TsmIsVector(), new TsmVectorLength(), new TsmVectorRef(), new TsmVectorSet(),
 
             new TsmCallWithValues(), new TsmValues(), new TsmApplyBuiltinNode(),
 
             new TsmOpenInputFile(), new TsmOpenOutputFile(),
             new TsmIsPort(), new TsmIsInputPort(), new TsmIsOutputPort(), new TsmIsBinaryPort(), new TsmIsTextualPort(), new TsmClosePort(),
-            new TsmIsEofObject(), new TsmCurrentInputPort(), new TsmCurrentOutputPort(), new TsmCurrentErrorPort(),
+            new TsmEofObject(), new TsmIsEofObject(), new TsmCurrentInputPort(), new TsmCurrentOutputPort(), new TsmCurrentErrorPort(),
+            new TsmDoesFileExist(),  new TsmDeleteFile(),
 
             new TsmReadBuiltinNode(), new TsmGetDatum(), new TsmWriteBuiltinNode(), new TsmPutDatum(),
 
@@ -143,14 +163,19 @@ public class TrustemeLanguage extends TruffleLanguage<TrustemeContext> {
         for (var bt : builtins)
             addBuiltin(topFrame, firstSlot++, bt);
 
-        loadPrims();
-//        loadExpander();
+        // order matters
+        loadFile(PRIMS);
+        loadFile(PSYNTAX);
 
         return topFrame;
     }
 
     public VirtualFrame getTopFrame() {
         return this.topFrame;
+    }
+
+    public Map<String, TsmExpr> getTopEnv() {
+        return this.topEnv;
     }
 
     private void addBuiltin(VirtualFrame frame, int slot, TsmBuiltinNode node) {
@@ -178,21 +203,6 @@ public class TrustemeLanguage extends TruffleLanguage<TrustemeContext> {
             var rootNode = new TsmEvalRootNode(this, mainNode, frame);
             rootNode.getCallTarget().call(topFrame);
         });
-    }
-
-    private final String PSYNTAX = "psyntax.pp";
-    private final String PRIMS = "prims.ss";
-
-    private void loadExpander() {
-        System.out.println("loading psyntax code");
-        loadFile(PSYNTAX);
-        System.out.println("loading psyntax code done");
-    }
-
-    private void loadPrims() {
-        System.out.println("loading primitives");
-        loadFile(PRIMS);
-        System.out.println("loading primitives done");
     }
 
     @Override
